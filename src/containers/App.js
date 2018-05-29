62152b880564a173222fb314e7eaf3427a2ddf78
Leonid Markov <mypochta75@gmail.com>
2018-05-30 00:02:29 +0300

    первый коммит

---
+++ src/containers/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+//import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+//import { Button, Input, FormGroup } from 'semantic-ui-react';
+import './App.css';
+
+
+class App extends Component {
+  
+  render() {
+    return (
+      <div className="App">
+      
+      </div>
+    );
+  }
+}
+
+export default connect(
+  state => ({});
+  dispatch => ({});
+)(App);
