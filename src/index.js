1fbbd90bda344193bff620d732c4a6d052855449
Leonid Markov <mypochta75@gmail.com>
2018-05-30 00:04:41 +0300

    первый коммит

--- src/index.js
+++ src/index.js
@@ -2,8 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './semantic/dist/semantic.min.css'
 import './index.css';
-import App from './App';
+import App from './containers/App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function contactlist(state = []) {
+	return state;
+}
+
+const store = createStore(contactlist);
+console.log(store.getState());
+
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(
+	<Provider store = {store}>
+		<App />
+	</Provider>, 
+	rootElement);
 registerServiceWorker();
